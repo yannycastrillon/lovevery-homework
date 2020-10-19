d44b6c6ab8a666f4c20b257187c484bef93dee1e
Yanny Castrillon <yannyandres117@gmail.com>
2020-10-18 19:07:06 -0500

    Create gift_controller code implementation to make all rspec passed and be green. Delegate the responsibility to application controller of all exception resolution handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    respond_with :not_found, exception
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    respond_with :record_invalid, exception
+  end
+
+  rescue_from Errors::ChildNotFound do |exception|
+    respond_with :not_found, exception
+  end
+
+  private
+
+  def respond_with(status, message)
+    render json: { errors: [message] }, status: status  
+  end
 end
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,25 +2,52 @@ class GiftsController < ApplicationController
   before_action :set_gift, only: :show
 
   def new
-    # ProductService PORO (product_id: )
-    # @product = Product.find(params[:product_id])
     @gift = Gift.new(gift_params)
   end
 
   def create
-  #  GiftService PORO (child_name: , birthdate: , parent_name)
+    result = BuildGiftService.call(child_attributes, gifter_attributes, gift_attributes)
+    raise result.error if result.is_a?(Failure)
+
+    gift = result.object
+
+    if gift.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
-private
+  private
 
   def set_gift
-    @gift = Gift.find_by(id: params[:id])
+    @gift ||= Gift.find(params[:id])
+  end
+
+  def child_attributes
+    child_params[:child].to_hash
+  end
+
+  def gifter_attributes
+    gifter_params[:gifter].to_hash
+  end
+
+  def gift_attributes
+    gift_params.to_hash
   end
 
   def gift_params
-    params.require(:gift).permit(:product_id, :child_name, :birthdate, :parent_name, :message)
+    params.require(:gift).permit(:product_id, :message)
+  end
+
+  def child_params
+    params.require(:gift).permit(child: [:full_name, :birthdate, :parent_name])
+  end
+
+  def gifter_params
+    params.require(:gift).permit(gifter: [:first_name, :last_name, :email])
   end
 end
